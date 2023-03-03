9f4cab9a58763c317d1eb0bd149347520ff5bd01
MarinaNguessan <marinanguessan23@gmail.com>
2023-03-03 17:31:03 +0000

    first commit

---
+++ src/exercice/Main.java
@@ -0,0 +1,18 @@
+package exercice;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+	
+	public static List<Integer> getDistinctNumbers(List<Integer> numbers) {
+        return numbers.stream().distinct().collect(Collectors.toList());
+    }
+
+	public static void main(String[] args) {
+		 List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+	        List<Integer> distinctNumbers = getDistinctNumbers(numbers);
+	        System.out.println(distinctNumbers);
+	}
+
+}
